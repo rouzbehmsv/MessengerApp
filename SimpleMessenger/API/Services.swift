fdba5817cf68b5e2c454fa67f0c3a1246175e324
rouzbehmsv <rouzbehmsv_1988@yahoo.com>
2018-10-16 13:20:34 +0330

    message Object Created

---
+++ SimpleMessenger/API/Services.swift
@@ -0,0 +1,18 @@
+//
+//  Services.swift
+//  SimpleMessenger
+//
+//  Created by Rouzbeh on 10/16/18.
+//  Copyright © 2018 Rouzbeh. All rights reserved.
+//
+
+import Foundation
+import Scaledrone
+
+class Services {
+    private let scaledrone: Scaledrone
+    private let messageCallback: (Message)-> Void
+    
+    
+}
+
---
+++ SimpleMessenger/Model/MessageObject.swift
@@ -0,0 +1,9 @@
+//
+//  MessageObject.swift
+//  SimpleMessenger
+//
+//  Created by Rouzbeh on 10/16/18.
+//  Copyright © 2018 Rouzbeh. All rights reserved.
+//
+
+import Foundation
